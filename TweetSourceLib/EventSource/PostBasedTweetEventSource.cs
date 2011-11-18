d0143ebb01c1911482895f916745ce1ec5457260
Natthawut Kulnirundorn <m3rlinez@gmail.com>
2011-11-18 07:39:49 +0700

    Revert "Now dumps raw output from Twitter server when encountered with WebException"
    This reverts commit bbe94b29c7e8e91a8d66fc589ee7cba0d27e12c1.

--- TweetSourceLib/EventSource/PostBasedTweetEventSource.cs
+++ TweetSourceLib/EventSource/PostBasedTweetEventSource.cs
@@ -47,7 +47,7 @@ namespace TweetSource.EventSource
             request.ContentType = "application/x-www-form-urlencoded";
 
             var header = CreateAuthHeader(request);
-            request.Headers.Add("Authorization", header.GetHeaderString() + "x");
+            request.Headers.Add("Authorization", header.GetHeaderString());
 
             using (var sw = new StreamWriter(request.GetRequestStream()))
             {
